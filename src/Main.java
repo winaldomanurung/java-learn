5c8d591f32b0539de3b1d8227c85f98c8cb53a04
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-14 15:53:19 +0700

    do-while loop

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,10 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		String name = "";
 		
-		while(name.isBlank()) {
+		do {
 			System.out.print("Enter your name: ");
 			name = scanner.nextLine();
-		}
+		} while(name.isBlank());
 		
 		System.out.println("Hello " + name);
 	}
