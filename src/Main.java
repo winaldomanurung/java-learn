3105ed1f3f306ea3de3bfcd5eaa8933b950a172d
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-14 15:29:30 +0700

    switch - conditional statements

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,47 @@
+import java.time.LocalDate;
 
 public class Main {
 	public static void main(String[] args) {
-		int age = 78;
-		if(age >= 18 & age <50 ) {
-			System.out.println("You are an adult!");
-		} else if (age > 50 ) {
-			System.out.println("You are an elderly!");
-		} else {
-			System.out.println("You are not an adult!");
-		} 
+		LocalDate today = LocalDate.now();
+		int month = today.getMonthValue();
+		
+		switch (month) {
+			case 1:
+				System.out.println("January");
+				break;
+			case 2:
+				System.out.println("February");
+				break;
+			case 3:
+				System.out.println("March");
+				break;
+			case 4:
+				System.out.println("April");
+				break;
+			case 5:
+				System.out.println("May");
+				break;
+			case 6:
+				System.out.println("June");
+				break;
+			case 7:
+				System.out.println("July");
+				break;
+			case 8:
+				System.out.println("August");
+				break;
+			case 9:
+				System.out.println("September");
+				break;
+			case 10:
+				System.out.println("October");
+				break;
+			case 11:
+				System.out.println("November");
+				break;
+			case 12:
+				System.out.println("December");
+				break;
+		}
 	}
 }
