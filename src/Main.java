d66b5a27a81b250b8233c61d475195e95076bd19
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-14 15:24:44 +0700

    if, else if, else - conditional statement

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,13 @@
 
 public class Main {
 	public static void main(String[] args) {
-		double x = 3.14;
-		double y = 36;
-		
-		double a = Math.max(x, y);
-		double b = Math.min(x, y);
-		double c = Math.abs(y);
-		double d = Math.sqrt(y);
-		double e = Math.round(x);
-		double f = Math.ceil(x);
-		
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(f);
+		int age = 78;
+		if(age >= 18 & age <50 ) {
+			System.out.println("You are an adult!");
+		} else if (age > 50 ) {
+			System.out.println("You are an elderly!");
+		} else {
+			System.out.println("You are not an adult!");
+		} 
 	}
 }
