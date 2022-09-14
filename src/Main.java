f41addb9ac2591ae92aa302b79050638d5ff8642
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-14 15:13:34 +0700

    make a GUI interface

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("What is your name?");
-		String name = scanner.nextLine();
-		System.out.println("Hello "+ name);
+		String name = JOptionPane.showInputDialog("Enter your name: ");
+		JOptionPane.showMessageDialog(null, "Hello "+ name);
+		
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+		JOptionPane.showMessageDialog(null, "You are "+ age + " years old.");
+		
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+		JOptionPane.showMessageDialog(null, "You are "+ height + " cm tall.");
 	}
 }
