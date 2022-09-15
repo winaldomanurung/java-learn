712f4da474a7159e4dfeee5447949afea6822725
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-15 17:08:41 +0700

    overloaded methods

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,27 @@
 
 public class Main {
 	public static void main(String[] args) {
-		String name = "Aldo";
-		int age = 21;
-		int x = 3;
-		int y = 4;
-		hello();
-		helloName(name);
-		helloNameAge(name, age);
-		int z = add(x,y);
+//		Overloaded Methods are methods that share the same name but have different parameters
+//		method name + parameters = method signature
+		int x= add (1,2);
+		int y= add (1,2,3);
+		int z= add (1,2,3,4);
+		
+		System.out.println(x);
+		System.out.println(y);
 		System.out.println(z);
 	}
-//		method is a block of code that is executed whenever it is called upon
 	
-	static void hello() {
-		System.out.println("Hello");		
+	static int add(int a, int b) {
+		System.out.println("This is overloaded method #1");
+		return a+b;
 	}
-	
-	static void helloName(String name) {
-		System.out.println("Hello "+ name);		
+	static int add(int a, int b, int c) {
+		System.out.println("This is overloaded method #2");
+		return a+b+c;
 	}
-
-	static void helloNameAge(String name, int age) {
-		System.out.println("Hello "+ name +". You are "+ age + " years old");	
-	}
-	
-	static int add(int x, int y) {
-		int z = x + y;
-		return z;
+	static int add(int a, int b, int c, int d) {
+		System.out.println("This is overloaded method #3");
+		return a+b+c+d;
 	}
 }
