36e045bf031ea4fc30c75e8ecb1425226719cd73
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-15 16:33:11 +0700

    2D arraylist

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,33 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-// 		ArrayList is a resizable array
-//		Elements can be added and removed after compilation phase 
-//		Only store reference data types
-		ArrayList<String> food = new ArrayList<String>();
-		food.add("pizza");
-		food.add("hamburger");
-		food.add("hotdog");
+// 		2D ArrayList is a dynamic list of lists
+//		We can change the size of these lists during runtime
 		
-		food.set(0, "sushi");
-		food.remove(2);
-		food.clear();
+		ArrayList<ArrayList<String>> groceryList = new ArrayList();
 		
-		for(int i=0;i<food.size();i++) {
-			System.out.println(food.get(i));
-		}
+		ArrayList<String> bakeryList = new ArrayList<String>();
+		bakeryList.add("pasta");
+		bakeryList.add("garlic bread");
+		bakeryList.add("donuts");
+		
+		ArrayList<String> produceList = new ArrayList<String>();
+		produceList.add("tomatoes");
+		produceList.add("zucchini");
+		produceList.add("peppers");
+		
+		ArrayList<String> drinksList = new ArrayList<String>();
+		drinksList.add("soda");
+		drinksList.add("coffee");
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(drinksList);
+		
+		System.out.println(groceryList);
+		System.out.println(groceryList.get(0));
+		System.out.println(groceryList.get(0).get(0));
+
 	}
 }
