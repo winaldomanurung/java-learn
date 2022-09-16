27433f9dd71fb043ae3b4cc23c86a38d2a97727d
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-16 15:40:58 +0700

    object passing as an argument

---
+++ src/Garage.java
@@ -0,0 +1,7 @@
+
+public class Garage {
+//	Kita pass argument berupa object Car
+	void park(Car car) {
+		System.out.println("The "+ car.name+ " is parked in garage.");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,11 @@
 
 public class Main {
 	public static void main(String[] args) {
-
-		Food[] refrigerator = new Food[3];
+		Garage garage = new Garage();
+		Car car1 = new Car("BMW");
+		Car car2 = new Car("Tesla");
 		
-		Food food1 = new Food("pizza");
-		Food food2 = new Food("hamburger");
-		Food food3 = new Food("hotdog");
-		
-		Food[] refrigerator2 = {food1, food2, food3};
-		
-		refrigerator[0] = food1;
-		refrigerator[1] = food2;
-		refrigerator[2] = food3;
-		
-		System.out.println(refrigerator[0]);
-//		hasilnya Food@5acf9800
-		
-		System.out.println(refrigerator[0].name);
-		System.out.println(refrigerator[1].name);
-		System.out.println(refrigerator[2].name);
-		System.out.println(refrigerator2[0].name);
-		System.out.println(refrigerator2[1].name);
-		System.out.println(refrigerator2[2].name);
+		garage.park(car1);
+		garage.park(car2);
 	}
 }
